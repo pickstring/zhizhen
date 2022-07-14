058b9c91612ad2a2facc92703146e379e6e7d81b
FL <FL@DESKTOP-08Q858E>
2022-07-14 18:13:35 +0800

    zhi

--- zhizhen.cpp
+++ zhizhen.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
-
+	//1、定义指针
+	int a = 10;
+	int* p;
+	//2、让指针记录a的地址
+	p = &a;
+	cout << (int)p << endl;
+	cout << (long long)p << endl;
+	cout << (int)&a << endl;
+	//3、使用指针--接引用
+	cout << *p << endl;
+	*p = 1000;
+	cout << a << endl;
 }
